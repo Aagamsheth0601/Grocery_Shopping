af88e0f52e92b4029ad4f61488f0f87d5e7b9756
Aagamsheth0601 <aagamsheth0601@gmail.com>
2021-02-05 11:35:28 +0530

    first commit

---
+++ Landing Page/demn.js
@@ -0,0 +1,25 @@
+// var fields = Document.getElementsByClassName('quantity');
+// var button1 = Document.getElementsByClassName('+button');
+// var button2 = Document.getElementsByClassName('-button');
+// var fieldValues;
+// for (let i = 0; i < fields.length; i++) {
+//   fieldValues[i] = parseInt(fields.length);
+// }
+
+// const addQuantity = () => {
+//   for (let i = 0; i < fieldValues.length; i++) {
+//     fieldValues[i]++;
+//     fields[i].placeholder = fieldValues[i].toString();
+//   }
+// };
+
+// const subtractQuantity = () => {
+//   for (let i = 0; i < fieldValues.length; i++) {
+//     if (fieldValues[i] > 0) fieldValues[i]--;
+//     fields[i].placeholder = fieldValues[i].toString();
+//   }
+// };
+
+// button1.addEventListener('click', addQuantity);
+// button2.addEventListener('click', subtractQuantity);
+
